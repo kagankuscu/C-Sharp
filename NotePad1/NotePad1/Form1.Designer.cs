176aa0360acd9346257495e5d9a1fdd615e988d0
kagankuscu <kagan-kuscu@hotmail.com.tr>
2017-04-27 22:10:02 +0300

    Not deneme

---
+++ NotePad1/NotePad1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace NotePad1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lstNotlar = new System.Windows.Forms.ListBox();
+            this.txtNot = new System.Windows.Forms.TextBox();
+            this.lblNotlar = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(12, 271);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 56);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(93, 271);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 56);
+            this.btnSil.TabIndex = 1;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 56);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // lstNotlar
+            // 
+            this.lstNotlar.FormattingEnabled = true;
+            this.lstNotlar.Location = new System.Drawing.Point(12, 38);
+            this.lstNotlar.Name = "lstNotlar";
+            this.lstNotlar.Size = new System.Drawing.Size(75, 225);
+            this.lstNotlar.TabIndex = 3;
+            this.lstNotlar.SelectedIndexChanged += new System.EventHandler(this.lstNotlar_SelectedIndexChanged);
+            // 
+            // txtNot
+            // 
+            this.txtNot.Location = new System.Drawing.Point(93, 38);
+            this.txtNot.Multiline = true;
+            this.txtNot.Name = "txtNot";
+            this.txtNot.Size = new System.Drawing.Size(156, 227);
+            this.txtNot.TabIndex = 4;
+            // 
+            // lblNotlar
+            // 
+            this.lblNotlar.AutoSize = true;
+            this.lblNotlar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNotlar.Location = new System.Drawing.Point(13, 13);
+            this.lblNotlar.Name = "lblNotlar";
+            this.lblNotlar.Size = new System.Drawing.Size(57, 20);
+            this.lblNotlar.TabIndex = 5;
+            this.lblNotlar.Text = "Notlar";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Not Ekleme";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnEkle;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnSil;
+            this.ClientSize = new System.Drawing.Size(265, 339);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblNotlar);
+            this.Controls.Add(this.txtNot);
+            this.Controls.Add(this.lstNotlar);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox lstNotlar;
+        private System.Windows.Forms.TextBox txtNot;
+        private System.Windows.Forms.Label lblNotlar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NotePad1/NotePad1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotePad1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            lstNotlar.Items.Add(txtNot.Text);
+            txtNot.Text = "";
+            txtNot.Focus();
+
+            lstNotlar.Items.Remove(lstNotlar.SelectedItem);
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            lstNotlar.Items.Remove(lstNotlar.SelectedItem);
+        }
+
+        private void lstNotlar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtNot.Text = lstNotlar.Text;
+            
+        }
+    }
+}
