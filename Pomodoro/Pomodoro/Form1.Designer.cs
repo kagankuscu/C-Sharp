e97792043f7d6beef99e18c0b5bc390d806bb48c
kagankuscu <kagan-kuscu@hotmail.com.tr>
2017-04-30 01:07:28 +0300

    Pomodoro
    Çalışıyor ama bir iki düzenleme var

---
+++ Pomodoro/Pomodoro/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Pomodoro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.lblDakika = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSaniye = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tmrCalisma = new System.Windows.Forms.Timer(this.components);
+            this.tmrDinlenme = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.BackColor = System.Drawing.Color.Lime;
+            this.btnBaslat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBaslat.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnBaslat.Location = new System.Drawing.Point(49, 179);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(75, 64);
+            this.btnBaslat.TabIndex = 0;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = false;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.BackColor = System.Drawing.Color.Red;
+            this.btnDurdur.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDurdur.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnDurdur.Location = new System.Drawing.Point(164, 179);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(75, 64);
+            this.btnDurdur.TabIndex = 1;
+            this.btnDurdur.Text = "Durdur";
+            this.btnDurdur.UseVisualStyleBackColor = false;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // lblDakika
+            // 
+            this.lblDakika.AutoSize = true;
+            this.lblDakika.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDakika.Location = new System.Drawing.Point(39, 40);
+            this.lblDakika.Name = "lblDakika";
+            this.lblDakika.Size = new System.Drawing.Size(36, 25);
+            this.lblDakika.TabIndex = 2;
+            this.lblDakika.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = ":";
+            // 
+            // lblSaniye
+            // 
+            this.lblSaniye.AutoSize = true;
+            this.lblSaniye.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaniye.Location = new System.Drawing.Point(110, 40);
+            this.lblSaniye.Name = "lblSaniye";
+            this.lblSaniye.Size = new System.Drawing.Size(36, 25);
+            this.lblSaniye.TabIndex = 4;
+            this.lblSaniye.Text = "00";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblDakika);
+            this.groupBox1.Controls.Add(this.lblSaniye);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(49, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(190, 97);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Zaman";
+            // 
+            // tmrCalisma
+            // 
+            this.tmrCalisma.Interval = 250;
+            this.tmrCalisma.Tick += new System.EventHandler(this.tmrCalisma_Tick);
+            // 
+            // tmrDinlenme
+            // 
+            this.tmrDinlenme.Interval = 250;
+            this.tmrDinlenme.Tick += new System.EventHandler(this.tmrDinlenme_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 299);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnDurdur);
+            this.Controls.Add(this.btnBaslat);
+            this.Name = "Form1";
+            this.Text = "Pomodoro";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Label lblDakika;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSaniye;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer tmrCalisma;
+        private System.Windows.Forms.Timer tmrDinlenme;
+    }
+}
+
---
+++ Pomodoro/Pomodoro/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+namespace Pomodoro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            tmrCalisma.Enabled = true;
+
+            lblDakika.Text = Dakika.ToString();
+            lblSaniye.Text = Saniye.ToString();
+
+            btnBaslat.BackColor = Color.Gainsboro;
+        }
+
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            tmrCalisma.Enabled = false;
+            tmrDinlenme.Enabled = false;
+
+            Dakika = 2;
+            DinlenmeDakika = 1;
+
+            lblDakika.ForeColor = Color.Black;
+            lblSaniye.ForeColor = Color.Black;
+            btnBaslat.BackColor = Color.Lime;
+
+            lblDakika.Text = "00";
+            lblSaniye.Text = "00";
+        }
+
+        int Dakika = 2, Saniye, DinlenmeDakika = 1;
+        private void tmrCalisma_Tick(object sender, EventArgs e)
+        {
+            switch (Saniye)
+            {
+                case 0:
+                    {
+                        Saniye = 60;
+                        Dakika--;
+                        lblDakika.Text = Dakika.ToString();
+                        break;
+                    }
+            }
+
+                Saniye--;
+                lblSaniye.Text = Saniye.ToString();
+                //if (Saniye == 0)
+                //{
+                    //Dakika--;
+                    lblDakika.Text = Dakika.ToString();
+                    //Saniye = 59;
+
+                if (Dakika < 0)
+                    {
+                        lblDakika.Text = "0";
+
+                        tmrCalisma.Enabled = false;
+                        Dakika = 2;
+
+                        SystemSounds.Beep.Play();
+                        MessageBox.Show("Biraz Dinlence Zamanı");
+                        tmrDinlenme.Enabled = true;
+
+                        this.BackColor = Color.SkyBlue;
+
+                        lblDakika.ForeColor = Color.Red;
+                        lblSaniye.ForeColor = Color.Red;
+
+                        lblDakika.Text = DinlenmeDakika.ToString();
+                        lblSaniye.Text = Saniye.ToString();
+                    }
+
+                //}
+        }
+
+        private void tmrDinlenme_Tick(object sender, EventArgs e)
+        {
+            switch (Saniye)
+            {
+                case 0:
+                    {
+                        Saniye = 60;
+                        Dakika--;
+                        lblDakika.Text = Dakika.ToString();
+                        break;
+                    }
+            }
+
+            Saniye--;
+            lblSaniye.Text = Saniye.ToString();
+            if (Saniye == 0)
+            {
+                DinlenmeDakika--;
+                lblDakika.Text = DinlenmeDakika.ToString();
+                Saniye = 59;
+
+                if (DinlenmeDakika < 0)
+                {
+                    tmrDinlenme.Enabled = false;
+                    DinlenmeDakika = 1;
+
+                    lblDakika.Text = "00";
+                    lblSaniye.Text = "00";
+                    lblDakika.ForeColor = Color.Black;
+                    lblSaniye.ForeColor = Color.Black;
+                    btnBaslat.BackColor = Color.Lime;
+
+                    SystemSounds.Beep.Play();
+                    MessageBox.Show("Pomodoro'yu Bitirdiğiniz");
+                }
+            }
+        }
+    }
+}
